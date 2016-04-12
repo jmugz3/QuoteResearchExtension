c5c7dbeed00f1e60591fa64aabe30f46f12a83fe
Jose Mugaburu <jose.mugaburu@gmail.com>
2016-04-12 16:14:12 -0400

    adds boilerplate code for consuming the API

---
+++ app.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var oauth = require('oauth').OAuth;
+var configuration = require('./tradekingConfig')();
+var xml2json = require('xml2json');
+
+var url = configuration.url;
+
+var requestToken = 'https://developers.tradeking.com/oauth/request_token';
+
+var userAuthorization = 'https://developers.tradeking.com/oauth/authorize';
+
+var accessToken = 'https://developers.tradeking.com/oauth/access_token';
+
+
+var tradeking_consumer = new oauth(
+  requestToken,
+  accessToken,
+  configuration.consumer_key,
+  configuration.consumer_secret,
+  "1.0",
+  null,
+  "HMAC-SHA1");
+
+
+function marketClock(error, callback){
+
+  tradeking_consumer.get(
+    configuration.api_url + '/market/clock.json', configuration.access_token,
+    configuration.access_secret,
+    function(error, data, response) {
+      if (error) console.error(error);
+      //var d = xml2json.toJson(data);
+      var d = JSON.parse(data);
+      console.log(d.response);
+      callback(d.response);
+    }
+  );
+
+  //console.log(d.response);
+}
+
+console.log(marketClock());
